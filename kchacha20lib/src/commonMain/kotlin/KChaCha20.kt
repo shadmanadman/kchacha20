0aaac2f1bd5154c31ad9943952bcddd977a76834
shadmanadman <adman.shadman@gmail.com>
2026-02-21 21:17:21 +0330

    Use AutoClosable

--- kchacha20lib/src/commonMain/kotlin/KChaCha20.kt
+++ kchacha20lib/src/commonMain/kotlin/KChaCha20.kt
@@ -2,24 +2,24 @@ import uniffi.chacha20.SecretCipher
 
 typealias EncryptedData = ByteArray
 
-object KChaCha20 {
+object KChaCha20 : AutoCloseable {
     private var secretCipher: SecretCipher? = null
 
     fun create(masterPassword: String, salt: String) {
-        destroy()
+        close()
 
         secretCipher = SecretCipher(masterPassword, salt)
     }
 
     fun encrypt(input: String): EncryptedData {
-        return secretCipher?.encrypt(input)?: error("KChaCha20 not initialized")
+        return secretCipher?.encrypt(input)?: error("KChaCha20 not initialized. Call create first.")
     }
 
     fun decrypt(encryptedData: EncryptedData): String {
-        return secretCipher?.decrypt(encryptedData)?: error("KChaCha20 not initialized")
+        return secretCipher?.decrypt(encryptedData)?: error("KChaCha20 not initialized. Call create first.")
     }
 
-    fun destroy(){
+    override fun close() {
         secretCipher?.destroy()
         secretCipher = null
     }
